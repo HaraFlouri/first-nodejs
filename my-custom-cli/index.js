fe54b611e356ccb26d50a32b64ed75eade7844bd
HaraFlouri <xaraflouri@gmail.com>
2024-06-20 14:23:59 +0200

    Building CLI Tools

---
+++ my-custom-cli/index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const yargs = require("yargs")
+const {argv} = yargs(process.argv)
+
+console.log(process.argv);
+
+const printByFiveMoves = async (pokemonName) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const pokemon = await response.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, 5));
+};
+
+printByFiveMoves(argv.pokemon);
